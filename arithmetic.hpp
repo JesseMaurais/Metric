531bc6709a41af76d6a09b62c1db91077ff59396
Jesse Maurais <jessemaurais@gmail.com>
2016-07-14 21:14:16 -0600

    Fixed error confusing max and mod in precision integer

--- arithmetic.hpp
+++ arithmetic.hpp
@@ -85,7 +85,8 @@ namespace arithmetic
 		using overflow = std::overflow_error;
 		using underflow = std::underflow_error;
 
-		static const uint_t mod = 1 + std::numeric_limits<base>::max();
+		static const uint_t max = std::numeric_limits<base>::max();
+		static const uint_t mod = max + 1;
 
 		std::array<base, size> digits;
 
@@ -96,7 +97,7 @@ namespace arithmetic
 			uint_t carry = 0;
 			for (size_t i = 0; i < size; ++i) {
 				uint_t num = digits[i] + that.digits[i] + carry;
-				if (mod < num) {
+				if (max < num) {
 					auto div = divide(num, mod);
 					digits[i] = div.rem;
 					carry = div.quot;	
@@ -136,7 +137,7 @@ namespace arithmetic
 			for (size_t i = 0; i < size; ++i) {
 			 for (size_t j = 0; j < size; ++j) {
 				uint_t num = digits[i] * that.digits[j] + carry;
-				if (mod < num) {
+				if (max < num) {
 					auto div = divide(num, mod);
 					sums[i + j] += div.rem;
 					carry = div.quot;
@@ -174,7 +175,7 @@ namespace arithmetic
 		integer operator++()
 		{
 			for (base &dig : digits) {
-				if (dig < mod) {
+				if (dig < max) {
 					++dig;
 					break;
 				}
@@ -190,7 +191,7 @@ namespace arithmetic
 					--dig;
 					break;
 				}
-				dig = mod;
+				dig = max;
 			}
 			return *this;
 		}
@@ -216,7 +217,7 @@ namespace arithmetic
 
 		bool operator!()
 		{
-			auto zero = [](base dig){ return !dig; };
+			auto zero = [](base dig){return !dig;};
 			return algorithm::all_of(digits, zero);
 		}
 
@@ -225,14 +226,14 @@ namespace arithmetic
 			digits.fill(0);
 			for (char byte : string) {
 				uint_t carry = byte - '0';
-				for (size_t index = 0; index < size; ++index) {
-					carry += digits[index] * 10;
-					if (mod < carry) {
+				for (size_t i = 0; i < size; ++i) {
+					carry += digits[i] * 10;
+					if (max < carry) {
 						auto div = divide(carry, mod);
-						digits[index] = div.rem;
+						digits[i] = div.rem;
 						carry = div.quot;
 					} else {
-						digits[index] = carry;
+						digits[i] = carry;
 						carry = 0;
 					}
 				}
@@ -261,6 +262,7 @@ namespace arithmetic
 
 		operator std::string() const
 		{
+			//TODO
 			std::string string;
 			for (base dig : digits) {
 				string += ":" + std::to_string(dig);
@@ -276,6 +278,7 @@ namespace arithmetic
 				num += order * digits[index];
 				order *= mod;
 				++index;
+				//should detect overflow
 			}
 			return num;
 		}
