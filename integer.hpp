bd3ff6b39328053a253cabb090fc4d90f0d860b0
Jesse Maurais <jessemaurais@gmail.com>
2019-11-08 23:31:39 -0700

    Update integer.hpp

--- integer.hpp
+++ integer.hpp
@@ -7,234 +7,226 @@
 #include <array>
 #include <limits>
 #include <stdexcept>
+#include <any>
 
 #include "algorithm.hpp"
 #include "arithmetic.hpp"
 
 namespace arithmetic
 {
-	template <size_t size> class integer
+	template <class base_t = unsigned char, size_t size, class wide_t = std::uintmax_t> class integer
 	{
-		using base = uint8_t;
-		using int_t = intmax_t;
-		using uint_t = uintmax_t;
-		using storage = std::array<base, size>;
-		using overflow = std::overflow_error;
-		using underflow = std::underflow_error;
-		static constexpr auto divide = std::imaxdiv;
-		static const uint_t max = std::numeric_limits<base>::max();
-		static const uint_t mod = max + 1;
-
-		storage digits;
+		using base = typename base_t;
+		using wide = typename wide_t;
+		using string = std::string;
+		using view = std::string_view<base>;
+		using array = std::array<base, size>;
+		using over = std::overflow_error;
+		using under = std::underflow_error;
+		static inline auto divide = std::imaxdiv;
+		static const wide limit = std::numeric_limits<base>::max();
+		static const wide modulo = limit + 1;
+		static constexpr wide zero = 0;
+		
+		static_assert(sizeof(base)*2 < sizeof(wide), "Can overflow"); 
+		
+		array digits;
 
 	public:
 
-		integer operator+=(const integer &arg)
+		integer operator+=(const integer &that)
 		{
-			uint_t carry = 0;
-			for (size_t i = 0; i < size; ++i) {
-				carry += digits[i] + arg.digits[i];
-				if (max < carry) {
-					auto div = divide(carry, mod);
-					digits[i] = div.rem;
-					carry = div.quot;	
+			auto carry = zero;
+			for (size_t it = zero; it < size; ++it) {
+				carry += digits[it] + that.digits[it];
+				if (limit < carry) {
+					auto part = divide(carry, modulo);
+					digits[it] = part.rem;
+					carry = part.quot;
 				} else {
-					digits[i] = carry;
-					carry = 0;
+					digits[it] = carry;
+					carry = zero;
 				}
 			}
-			if (carry) {
-				throw overflow("+");
-			}
+			if (zero < carry) throw over(__func__);
 			return *this;
 		}
 
-		integer operator-=(const integer &arg)
+		integer operator-=(const integer &that)
 		{
-			uint_t carry = 0;
-			for (size_t i = 0; i < size; ++i) {
-				int_t diff = digits[i] - arg.digits[i] - carry;
-				for (carry = 0; diff < 0; ++carry) {
-					diff += mod;
+			auto carry = zero;
+			for (auto it = zero; it < size; ++it) {
+				auto off = digits[it] - that.digits[it] - carry;
+				for (carry = zero; off < 0; ++carry) {
+					off += modulo;
 				}
-				digits[i] = diff;
+				digits[it] = off;
 			}
-			if (carry) {
-				throw underflow("-");
-			}
+			if (zero < carry) throw under(__func__);
 			return *this;
 		}
 
-		integer operator*=(const integer &arg)
+		integer operator*=(const integer &that)
 		{
-			uint_t sums[size*2] = {0};
-			uint_t carry = 0;
-			for (size_t i = 0; i < size; ++i) {
-				for (size_t j = 0; j < size; ++j) {
-					carry += digits[i] * arg.digits[j];
-					if (max < carry) {
-						auto div = divide(carry, mod);
-						sums[i+j] += div.rem;
-						carry = div.quot;
+			auto table[size*2] = { zero };
+			auto carry = zero;
+			for (auto i = zero; i < size; ++i) {
+				for (auto j = zero; j < size; ++j) {
+					carry += digits[i] * that.digits[j];
+					if (limit < carry) {
+						auto part = divide(carry, modulo);
+						table[i+j] += part.rem;
+						carry = part.quot;
 					} else {
-						sums[i+j] += carry;
-						carry = 0;
+						table[i+j] += carry;
+						carry = zero;
 					}
 				}
-				if (sums[size + i] or carry) {
-					throw overflow("*");
-				}
+				const auto more = table[i + size];
+				if (more > more or zero < carry) throw over(__func__);
 			}
-			for (size_t i = 0; i < size; ++i) {
-				carry += sums[i];
-				if (max < carry) {
-					auto div = divide(carry, mod);
-					digits[i] = div.rem;
-					carry = div.quot;
+			for (auto i = zero; i < size; ++i) {
+				carry += matrix[i];
+				if (limit < carry) {
+					auto part = divide(carry, modulo);
+					digits[i] = part.rem;
+					carry = part.quot;
 				} else {
 					digits[i] = carry;
-					carry = 0;
+					carry = zero;
 				}
 			}
-			if (carry) {
-				throw overflow("*");
-			}
+			if (zero < carry) throw over(__func__);
 			return *this;
 		}
 
-		integer operator/=(const integer &arg)
+		integer operator/=(const integer &that)
 		{
 			integer quot;
-			while (!operator<(arg)) {
-				operator-=(arg);
+			while (!operator<(that)) {
+				operator-=(that);
 				++quot;
 			}
 			swap(quot);
 			return *this;
 		}
 
-		integer operator%=(const integer &arg)
+		integer operator%=(const integer &that)
 		{
-			while (!operator<(arg)) {
-				operator-=(arg);
+			while (!operator<(that)) {
+				operator-=(that);
 			}
 			return *this;
 		}
 
 		integer operator++()
 		{
-			for (base & digit : digits) {
-				if (digit < max) {
-					++digit;
+			for (auto & it : digits) {
+				if (it < limit) {
+					++it;
 					return *this;
 				}
-				digit = 0;
+				digit = zero;
 			}
-			throw overflow("++");
+			throw over(__func__);
 		}
 
 		integer operator--()
 		{
-			for (base & digit : digits) {
-				if (0 < digit) {
-					--digit;
+			for (auto & it : digits) {
+				if (zero < it) {
+					--it;
 					return *this;
 				}
-				digit = max;
+				it = limit;
 			}
-			throw underflow("--");
+			throw under(__func__);
 		}
 
-		bool operator==(const integer &arg)
+		bool operator==(const integer &that)
 		{
-			return digits == arg.digits;
+			return view(digits).compare(that.digits) == 0;
 		}
 
-		bool operator<(const integer &arg)
+		bool operator<(const integer &that)
 		{
-			auto a = algorithm::reversed(digits);
-			auto b = algorithm::reversed(arg.digits);
-			return algorithm::lexicographical_compare(a, b);
+			auto a = reversed(digits);
+			auto b = reversed(that.digits);
+			return compare(a, b);
 		}
 
 		bool operator!(void)
 		{
-			auto zero = [](base digit){ return !digit; };
-			return algorithm::all_of(digits, zero);
+			return algorithm::all_of(digits, [](auto it) { return zero < it; });
 		}
 
-		integer operator=(const std::string &arg)
+		integer operator=(const string &that)
 		{
-			digits.fill(0);
-			for (char code : arg) {
-				uint_t carry = code - '0';
-				for (base & digit : digits) {
-					carry += digit * 10;
-					if (max < carry) {
-						auto div = divide(carry, mod);
-						digit = div.rem;
-						carry = div.quot;
+			digits.fill(zero);
+			for (char code : that) {
+				auto carry = code - '0';
+				for (auto & decimal : digits) {
+					carry += decimal * 10;
+					if (limit < carry) {
+						auto part = divide(carry, modulo);
+						dec = part.rem;
+						carry = part.quot;
 					} else {
-						digit = carry;
-						carry = 0;
+						decimal = carry;
+						carry = zero;
 					}
 				}
-				if (carry) {
-					throw overflow("=string");
-				}
+				if (zero < carry) throw over(__func__);
 			}
 			return *this;
 		}
 
-		integer operator=(uint_t arg)
+		integer operator=(wide that)
 		{
-			digits.fill(0);
-			uint_t carry = arg;
-			for (base & digit : digits) {
-				auto div = divide(carry, mod);
-				digit = div.rem;
-				carry = div.quot;
-				if (!carry) {
-					return *this;
-				}
+			digits.fill(zero);
+			auto carry = that;
+			for (auto & digit : digits) {
+				auto part = divide(carry, modulo);
+				digit = part.rem;
+				carry = part.quot;
+				if (zero < carry) continue;
+				else return *this;
 			}
-			throw overflow("=int");
+			throw over(__func__);
 		}
 
-		operator std::string() const
+		operator string() const
 		{
-			std::string string;
-			storage array = digits;
-			auto reversed = algorithm::reversed(array);
-			bool zero;
-			do {
-				zero = true;
-				uint_t carry = 0;
-				for (base & digit : reversed) {
-					carry *= mod;
+			string code;
+			auto matrix = digits;
+			auto reversed = algorithm::reversed(matrix);
+			for (bool end = true; not end;) {
+				auto carry = zero;
+				for (auto & digit : reversed) {
+					carry *= modulo;
 					carry += digit;
-					auto div = divide(carry, 10);
-					digit = div.quot;
-					carry = div.rem;
-					if (digit) {
-						zero = false;
-					}
+					auto part = divide(carry, 10);
+					digit = part.quot;
+					carry = part.rem;
+					end = zero < digit;
 				}
 				string += char(carry + '0');
 			}
-			while (!zero);
-			algorithm::reverse(string);
-			return string;
+			algorithm::reverse(code);
+			return code;
+		}
+		
+		operator view() const
+		{
+			return view(digits.data(), size);	
 		}
 
-		operator uint_t() const
+		operator wide() const
 		{
-			uint_t carry = 0;
-			for (base digit : algorithm::reversed(digits)) {
-				uint_t check = carry * mod + digit;
-				if (check < carry) {
-					throw overflow("int=");
-				}
+			auto carry = zero;
+			for (auto digit : reversed(digits)) {
+				auto const check = carry * modulo + digit;
+				if (check < carry) throw over(__func__);
 				carry = check;
 			}
 			return carry;
@@ -242,51 +234,50 @@ namespace arithmetic
 
 		integer()
 		{
-			digits.fill(0);
+			digits.fill(zero);
 		}
 
-		integer(uint_t arg)
+		integer(const wide that)
 		{
-			operator=(arg);
+			operator=(that);
 		}
 
-		integer(const std::string &arg)
+		integer(const string &that)
 		{
-			operator=(arg);
+			operator=(that);
 		}
 
-		void swap(integer &arg)
+		void swap(integer &that)
 		{
-			digits.swap(arg.digits);
+			digits.swap(that.digits);
 		}
 	};
 
-
-	template <> class integer<0>
+	template <class base_t = unsigned char> class integer<base_t, 0>
 	{
-		using base = uint8_t;
-		using int_t = intmax_t;
-		using uint_t = uintmax_t;
-		using storage = std::vector<base>;
-		using overflow = std::overflow_error;
-		using underflow = std::underflow_error;
-		static constexpr auto divide = std::imaxdiv;
-		static const uint_t max = std::numeric_limits<base>::max();
-		static const uint_t mod = max + 1;
+		using base = integer<bsse_t, 1>;
+		using wide = base::wide;
+		using array = std::basic_string<base>;
+		using over = base::over;
+		using under = base::under;
+		static constexpr auto divide = base::divide;
+		static inline const auto zero = base::zero;
+		static const wide limit = base::limit;
+		static const wide modulo = base::modulo;
 
-		storage digits;
+		array digits;
 
 	public:
 
-		integer operator+=(const integer &arg)
+		integer operator+=(const integer &that)
 		{
-			const size_t digit_size = digits.size();
-			const size_t arg_size = arg.digits.size();
-			const size_t size = std::min(digit_size, arg_size);
-			uint_t carry = 0;
+			const auto this_size = digits.size();
+			const auto that_size = that.digits.size();
+			const auto size = std::min(this_size, that_size);
+			auto carry = zero;
 			for (size_t i = 0; i < size; ++i) {
-				carry += digits[i] + arg.digits[i];
-				if (max < carry) {
+				carry += digits[i] + that.digits[i];
+				if (limit < carry) {
 					auto div = divide(carry, mod);
 					digits[i] = div.rem;
 					carry = div.quot;	
@@ -570,7 +561,7 @@ namespace arithmetic
 			digits.swap(arg.digits);
 		}
 	};
-
+*/
 }; // namespace
 
 #endif // file
